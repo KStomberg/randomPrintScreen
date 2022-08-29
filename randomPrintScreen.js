482da7caeb96add1491e25987ece667bce124742
KStomberg <68118089+KStomberg@users.noreply.github.com>
2022-08-29 17:13:33 -0500

    Code was ugly, was bored

--- randomPrintScreen.js
+++ randomPrintScreen.js
@@ -1,14 +1,10 @@
 //Function generates a random prnt.sc url to snoop on people because this is what I do now I guess
 
 function randomPrintScreen() {
-  let l1 = String.fromCharCode(97+Math.floor(Math.random() * 26));
-  let l2 = String.fromCharCode(97+Math.floor(Math.random() * 26));
-  let n1 = Math.floor(Math.random() * 9).toString();
-  let n2 = Math.floor(Math.random() * 9).toString();
-  let n3 = Math.floor(Math.random() * 9).toString();
-  let n4 = Math.floor(Math.random() * 9).toString();
-
-  let randomString = l1+l2+n1+n2+n3+n4;
-  
-  return `https://prnt.sc/${randomString}`
+  let slug = 'https://prnt.sc/';
+  for (let i = 0; i <= 1; i++) {
+    slug += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  }
+  slug += Math.floor(1000 + Math.random() * 9000);
+  return slug;
 }
